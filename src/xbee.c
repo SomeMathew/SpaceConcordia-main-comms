3176ccc818355fd9553c22febb73a736175ca2c4
SomeMatthew <SomeMathew@outlook.com>
2017-02-21 16:10:58 -0500

    removed the AT command stuff, not needed

--- src/xbee.c
+++ src/xbee.c
@@ -1,12 +1,9 @@
 /**
- * @brief Asynchronous xbee driver implementation
+ * @brief XBEE driver implementation
  * 
  * The driver uses the UART driver to enqueue new data for transmission in
  * transparant mode, this part is handled transparently by the UART
  * driver.
- * 
- * For AT Command modes, the module uses the task scheduler and a 
- * circular buffer to handle the guard time requirements
  */ 
 
 #include <stdbool.h>
@@ -21,26 +18,7 @@
 #define XBEE_CONF_STOPBITS UART_STOPBITS_1
 #define XBEE_CONF_PARITY UART_PARITY_NONE
 
-//~ #define XBEE_MODULE_CONF_BAUDRATE 0x06
-
-//~ #define XBEE_MODULE_IOCTL_CMD "+++"
-//~ #define XBEE_MODULE_IOCTL_CMD_LEN 3
-//~ #define XBEE_MODULE_IOCTL_GUARDTIME 1000
-
-//~ #define IOCTL_BUFFER_SIZE 128
-//~ static const char IOCTL_BUFFER_SEPARATOR = '#'
-
 static McuDevice_UART xbeeUartDevice = NULL;
-
-//~ static struct task * waitTask = NULL; 
-//~ static bool inATMode = false;
-//~ struct circularBuffer bufferATCMD;
-//~ uint8_t bufferATCMDArray[IOCTL_BUFFER_SIZE];
-
-//~ static void xbeeInitConfig();
-//~ static void enterATMode();
-//~ static void asyncATPRocess(uint32_t event, void * args);
-//~ static void sendATCommand(char * command, uint8_t * param);
 
 int xbee_open(McuDevice_UART uartDevice) {
 	if (xbeeUartDevice != NULL) {
@@ -59,11 +37,7 @@ int xbee_open(McuDevice_UART uartDevice) {
 	} 
 
 	xbeeUartDevice = uartDevice;
-	//~ buffer_attachArray(&bufferATCMD, bufferATCMDArray, LENGTH_OF_ARRAY(bufferATCMDArray));
-	
-	
-	//~ xbeeInitConfig();
-	
+
 	return DRIVER_STATUS_OK;
 }
 
@@ -73,10 +47,6 @@ int xbee_open(McuDevice_UART uartDevice) {
  * 		Clear and remove the buffers
  */
 int xbee_close() {
-	//~ if (xbeeUartDevice == NULL) {
-		//~ return DRIVER_STATUS_ERROR;
-	//~ }
-	//~ uart_close(
 	return DRIVER_STATUS_ERROR; //TODO method stub
 }
 
@@ -87,31 +57,3 @@ int xbee_write(uint8_t * data, size_t size) {
 	uart_write(xbeeUartDevice, data, size);
 	return DRIVER_STATUS_OK;
 }
-
-//~ static void xbeeInitConfig() {
-	//~ // TODO function stub
-//~ }
-
-//~ /**
- //~ * @brief send command and param in AT Command mode asynchronously.
- //~ * 
- //~ * @param command null-terminated ASCII string
- //~ * @param param Hex Parameter
- //~ */
-//~ static sendATCommand(char * command, uint8_t * param, size_t paramSize) {
-	//~ buffer_enqueue(&bufferATCMD, (uint8_t *) &IOCTL_BUFFER_SEPARATOR, 1);
-	//~ buffer_enqueue(&bufferATCMD, (uint8_t *) 
-	
-	//~ enterATMode();
-//~ }
-
-//~ static void enterATMode() {
-	//~ xbee_write(XBEE_MODULE_IOCTL_CMD, XBEE_MODULE_IOCTL_CMD_LEN);
-	//~ waitTask = createTask(asyncInitConf, 0, NULL, 
-	
-//~ }
-
-//~ static void asyncATPRocess(uint32_t event, void * args) {
-	//~ void (*callback)() = (void (*)()) args;
-	
-//~ }
