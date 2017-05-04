c6d50e53404dd4740506436fd5dbf9b9400be9e4
Alessandro Power <alessandrojpower@gmail.com>
2017-05-04 18:54:52 -0400

    Moved capacity definition to data gatherer c file.

--- inc/dataGatherer.h
+++ inc/dataGatherer.h
@@ -10,22 +10,4 @@
 #ifndef DATAGATHERER_H_
 #define DATAGATHERER_H_
 
-#include <stddef.h>
-#include <stdint.h>
-#include "acquisitionBuffers.h"
-
-#define PACKET_BUFF_CAPACITY ( \
-		ACQBUFF_TIMESTAMP_BUFF_CAPACITY + \
-		ACQBUFF_PITOT_BUFF_CAPACITY + \
-		ACQBUFF_BAROMETER_BUFF_CAPACITY + \
-		ACQBUFF_GPSALTITUDE_BUFF_CAPACITY + \
-		ACQBUFF_GPSPOSITION_BUFF_CAPACITY + \
-		ACQBUFF_ACCELEROMETER_BUFF_CAPACITY + \
-		ACQBUFF_GYROSCOPE_BUFF_CAPACITY )
-
-/*
- * TODO add documentation
- */
-size_t read_telem(uint8_t * buffer);
-
 #endif /* DATAGATHERER_H_ */
