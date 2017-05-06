fa813e79649a0e639b2a284752379318b7dc1ab7
Alessandro Power <alessandrojpower@gmail.com>
2017-05-06 09:16:28 -0400

    Added functions to send telem data to xbee.

--- src/dataGatherer.c
+++ src/dataGatherer.c
@@ -9,18 +9,24 @@
 #include "acquisitionBuffers.h"
 #include "dataGatherer.h"
 #include "sysTimer.h"
+#include "xbee.h"
 
-#define TELEM_PACKET_BUFF_CAPACITY                     \
-		(ACQBUFF_TIMESTAMP_BUFF_CAPACITY     + \
-		 ACQBUFF_PITOT_BUFF_CAPACITY         + \
-		 ACQBUFF_BAROMETER_BUFF_CAPACITY     + \
-		 ACQBUFF_GPSALTITUDE_BUFF_CAPACITY   + \
-		 ACQBUFF_GPSPOSITION_BUFF_CAPACITY   + \
-		 ACQBUFF_ACCELEROMETER_BUFF_CAPACITY + \
-		 ACQBUFF_GYROSCOPE_BUFF_CAPACITY)
+#define TELEM_PACKET_BUFF_CAPACITY                                                 \
+	(ACQBUFF_TIMESTAMP_BUFF_CAPACITY + ACQBUFF_PITOT_BUFF_CAPACITY +           \
+	 ACQBUFF_BAROMETER_BUFF_CAPACITY + ACQBUFF_GPSALTITUDE_BUFF_CAPACITY +     \
+	 ACQBUFF_GPSPOSITION_BUFF_CAPACITY + ACQBUFF_ACCELEROMETER_BUFF_CAPACITY + \
+	 ACQBUFF_GYROSCOPE_BUFF_CAPACITY)
 
+static size_t  telem_packet_buff_size;
 static uint8_t telem_packet_buff[TELEM_PACKET_BUFF_CAPACITY];
-static size_t read_telem_data() {
+
+static void read_telem_data(void);
+static int  send_telem_xbee(void);
+static void read_and_send_telem(uint32_t, void *);
+
+void data_gatherer_init(void);
+
+static void read_telem_data(void) {
 	const AcqBuff_Buffer buffers[] = {acqbuff_Pitot,
 	                                  acqbuff_Barometer,
 	                                  acqbuff_GPSAltitude,
@@ -32,9 +38,7 @@ static size_t read_telem_data() {
 
 	// Add msTick.
 	const uint32_t time = sysTimer_GetTick();
-	end += sprintf(end, "%u,", time) - 1; // Subtraction to remove null
-	                                      // character.
-	*end++ = ',';
+	end += sprintf(end, "%u,", time);
 
 	// Iterate over all acquisition buffers and read them into the
 	// telemetry packet buffer, separating the contents of each buffer with
@@ -47,5 +51,14 @@ static size_t read_telem_data() {
 	// Replace the trailing comma with a newline.
 	*(end - 1) = '\n';
 
-	return end - telem_packet_buff;
+	telem_packet_buff_size = end - telem_packet_buff;
+}
+
+static int send_telem_xbee(void) {
+	return xbee_write(telem_packet_buff, telem_packet_buff_size);
+}
+
+static void read_and_send_telem(uint32_t, void *) {
+	read_telem_data();
+	send_telem_xbee();
 }
