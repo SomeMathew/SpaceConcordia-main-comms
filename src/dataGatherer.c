c87bf9883b6f2e93511c9f0c42a8319ca2926dd8
Alessandro Power <alessandrojpower@gmail.com>
2017-05-05 22:37:43 -0400

    Styled read_telem_data.

--- src/dataGatherer.c
+++ src/dataGatherer.c
@@ -10,30 +10,30 @@
 #include "dataGatherer.h"
 #include "sysTimer.h"
 
-#define TELEM_PACKET_BUFF_CAPACITY ( \
-		ACQBUFF_TIMESTAMP_BUFF_CAPACITY + \
-		ACQBUFF_PITOT_BUFF_CAPACITY + \
-		ACQBUFF_BAROMETER_BUFF_CAPACITY + \
-		ACQBUFF_GPSALTITUDE_BUFF_CAPACITY + \
-		ACQBUFF_GPSPOSITION_BUFF_CAPACITY + \
-		ACQBUFF_ACCELEROMETER_BUFF_CAPACITY + \
-		ACQBUFF_GYROSCOPE_BUFF_CAPACITY )
+#define TELEM_PACKET_BUFF_CAPACITY                     \
+		(ACQBUFF_TIMESTAMP_BUFF_CAPACITY     + \
+		 ACQBUFF_PITOT_BUFF_CAPACITY         + \
+		 ACQBUFF_BAROMETER_BUFF_CAPACITY     + \
+		 ACQBUFF_GPSALTITUDE_BUFF_CAPACITY   + \
+		 ACQBUFF_GPSPOSITION_BUFF_CAPACITY   + \
+		 ACQBUFF_ACCELEROMETER_BUFF_CAPACITY + \
+		 ACQBUFF_GYROSCOPE_BUFF_CAPACITY)
 
 static uint8_t telem_packet_buff[TELEM_PACKET_BUFF_CAPACITY];
-static size_t read_telem_data(void) {
-	const AcqBuff_Buffer buffers[] = {
-		acqbuff_Pitot;
-		acqbuff_Barometer,
-		acqbuff_GPSAltitude,
-		acqbuff_GPSPosition,
-		acqbuff_Accelerometer,
-		acqbuff_Gyroscope
-	};
-	uint8_t * end = telem_packet_buff; // Points past the end of the buffer.
+static size_t read_telem_data() {
+	const AcqBuff_Buffer buffers[] = {acqbuff_Pitot,
+	                                  acqbuff_Barometer,
+	                                  acqbuff_GPSAltitude,
+	                                  acqbuff_GPSPosition,
+	                                  acqbuff_Accelerometer,
+	                                  acqbuff_Gyroscope};
+	uint8_t* end = telem_packet_buff; // Points past the last filled
+	                                  // element of the buffer.
 
 	// Add msTick.
 	const uint32_t time = sysTimer_GetTick();
-	end += sprintf(end, "%u,", time) - 1; // Subtraction to remove nul character.
+	end += sprintf(end, "%u,", time) - 1; // Subtraction to remove null
+	                                      // character.
 
 	// Iterate over all acquisition buffers and read them into the
 	// telemetry packet buffer, separating the contents of each buffer with
