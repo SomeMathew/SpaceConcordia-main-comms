02450fe6e7dfe2a04a756e8e1400d9cfd0bd016c
Alessandro Power <alessandrojpower@gmail.com>
2017-06-01 18:30:13 -0400

    Implemented spi_handle init for SPI.

---
+++ inc/pitot.h
@@ -0,0 +1,17 @@
+/**
+ * file: pitot.h
+ * TODO add description
+ */
+#ifndef PITOT_H
+#define PITOT_H
+
+#include <stdbool.h>
+#include "stm32f1xx_hal_conf"
+
+/**
+ * Initializes the HAL device struct. Returns true if initialization was
+ * successful.
+ */
+bool init_pitot(void);
+
+#endif
---
+++ src/pitot.c
@@ -0,0 +1,21 @@
+/**
+ * file: pitot.c
+ * TODO add description
+ */
+#include "pitot.h"
+
+void init_pitot(void) {
+	SPI_InitTypeDef spi_handle = { 0 };
+
+	spi_init.Mode        = SPI_MODE_MASTER;
+	spi_init.Direction   = SPI_DIRECTION_1LINE;
+	spi_init.DataSize    = SPI_DATASIZE_16BIT;
+	spi_init.CLKPolarity = SPI_POLARITY_HIGH;
+	spi_init.CLKPhase    = SPI_PHASE1_EDGE;
+	spi_init.FirstBit    = SPI_FIRSTBIT_MSB;
+
+	if (HAL_SPI_INIT(&spi_handle) != HAL_OK) {
+		return false;
+	};
+	return true;
+}
