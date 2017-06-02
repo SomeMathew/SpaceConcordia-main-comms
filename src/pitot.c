58b586788f2ef343c6222dc71b5c745939710dd6
Alessandro Power <alessandrojpower@gmail.com>
2017-06-02 07:31:22 -0400

    Corrected typo in variable name.

--- src/pitot.c
+++ src/pitot.c
@@ -7,12 +7,12 @@
 void init_pitot(void) {
 	SPI_InitTypeDef spi_handle = { 0 };
 
-	spi_init.Mode        = SPI_MODE_MASTER;
-	spi_init.Direction   = SPI_DIRECTION_1LINE;
-	spi_init.DataSize    = SPI_DATASIZE_16BIT;
-	spi_init.CLKPolarity = SPI_POLARITY_HIGH;
-	spi_init.CLKPhase    = SPI_PHASE1_EDGE;
-	spi_init.FirstBit    = SPI_FIRSTBIT_MSB;
+	spi_handle.Mode        = SPI_MODE_MASTER;
+	spi_handle.Direction   = SPI_DIRECTION_1LINE;
+	spi_handle.DataSize    = SPI_DATASIZE_16BIT;
+	spi_handle.CLKPolarity = SPI_POLARITY_HIGH;
+	spi_handle.CLKPhase    = SPI_PHASE1_EDGE;
+	spi_handle.FirstBit    = SPI_FIRSTBIT_MSB;
 
 	return HAL_SPI_INIT(&spi_handle) == HAL_OK:
 }
