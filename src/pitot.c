54f805b1283e96f66f0f622078b71c232af4928f
Alessandro Power <alessandrojpower@gmail.com>
2017-06-04 19:03:31 -0400

    Converted SPI_InitTypeDef to SPI_HandleTypeDef.

--- src/pitot.c
+++ src/pitot.c
@@ -4,15 +4,16 @@
  */
 #include "pitot.h"
 
-void init_pitot(void) {
-	SPI_InitTypeDef spi_handle = { 0 };
+bool init_pitot(void) {
+	SPI_HandleTypeDef spi_handle = {0};
 
-	spi_handle.Mode        = SPI_MODE_MASTER;
-	spi_handle.Direction   = SPI_DIRECTION_1LINE;
-	spi_handle.DataSize    = SPI_DATASIZE_16BIT;
-	spi_handle.CLKPolarity = SPI_POLARITY_HIGH;
-	spi_handle.CLKPhase    = SPI_PHASE1_EDGE;
-	spi_handle.FirstBit    = SPI_FIRSTBIT_MSB;
+	spi_handle.Instance         = SPIx;
+	spi_handle.Init.Mode        = SPI_MODE_MASTER;
+	spi_handle.Init.Direction   = SPI_DIRECTION_1LINE;
+	spi_handle.Init.DataSize    = SPI_DATASIZE_16BIT;
+	spi_handle.Init.CLKPolarity = SPI_POLARITY_HIGH;
+	spi_handle.Init.CLKPhase    = SPI_PHASE1_EDGE;
+	spi_handle.Init.FirstBit    = SPI_FIRSTBIT_MSB;
 
 	return HAL_SPI_INIT(&spi_handle) == HAL_OK:
 }
