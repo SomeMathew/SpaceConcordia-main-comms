2198ce2c60e878344516057ef2f046dee41053f8
Alessandro Power <alessandrojpower@gmail.com>
2017-06-05 15:50:28 -0400

    Made spi_handle a global static stuct. Set baudrate prescaler and nss.

--- src/pitot.c
+++ src/pitot.c
@@ -1,19 +1,22 @@
-/**
+/*
  * file: pitot.c
  * TODO add description
  */
 #include "pitot.h"
 
-bool init_pitot(void) {
-	SPI_HandleTypeDef spi_handle = {0};
+static SPI_HandleTypeDef spi_handle;
 
-	spi_handle.Instance         = SPIx;
-	spi_handle.Init.Mode        = SPI_MODE_MASTER;
-	spi_handle.Init.Direction   = SPI_DIRECTION_1LINE;
-	spi_handle.Init.DataSize    = SPI_DATASIZE_16BIT;
-	spi_handle.Init.CLKPolarity = SPI_POLARITY_HIGH;
-	spi_handle.Init.CLKPhase    = SPI_PHASE1_EDGE;
-	spi_handle.Init.FirstBit    = SPI_FIRSTBIT_MSB;
+bool init_pitot(void) {
+	spi_handle.Instance               = SPIx;
+	spi_handle.Init.Mode              = SPI_MODE_MASTER;
+	spi_handle.Init.Direction         = SPI_DIRECTION_1LINE;
+	spi_handle.Init.DataSize          = SPI_DATASIZE_16BIT;
+	spi_handle.Init.CLKPolarity       = SPI_POLARITY_HIGH;
+	spi_handle.Init.CLKPhase          = SPI_PHASE1_EDGE;
+	spi_handle.Init.NSS               = SPI_HARD_OUTPUT;            // pls check this.
+	spi_handle.Init.BaudRatePrescaler = SPI_BAUDRATE_PRESCALER_256; // and this
+	spi_handle.Init.FirstBit          = SPI_FIRSTBIT_MSB;
+	spi_handle.Init.TIMode            = SPI_TIMODE_DISABLE;
 
 	return HAL_SPI_INIT(&spi_handle) == HAL_OK:
 }
